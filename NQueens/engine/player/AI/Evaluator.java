ec7d36689d8f903bdb49b238e5d99141b5d3ff0e
Andres Carrillo <andresc@cpp.edu>
2021-09-12 22:13:44 -0700

    Add files via upload

---
+++ NQueens/engine/player/AI/Evaluator.java
@@ -0,0 +1,11 @@
+package com.chess.engine.player.AI;
+
+import com.chess.engine.board.Board;
+import com.chess.engine.peices.Alliance;
+
+public final class Evaluator {
+  
+	public static long evaluate(final Board board,final Alliance side) {
+	  return (Long) null;
+  }
+}
---
+++ NQueens/engine/player/AI/MiniMax.java
@@ -0,0 +1,11 @@
+package com.chess.engine.player.AI;
+
+import com.chess.engine.board.Board;
+
+public class MiniMax {
+	private int depth;
+	private Board curBoard;
+	private long alpha;
+	private long beta;
+	
+}
