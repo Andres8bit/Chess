4ca2b3ab2cb0dc7ba508e7ede1cf5596a1e7caba
Andres Carrillo <andresc@cpp.edu>
2021-09-15 18:35:55 -0700

    Add files via upload

--- NQueens/engine/player/AI/Evaluator.java
+++ NQueens/engine/player/AI/Evaluator.java
@@ -1,11 +1,65 @@
 package com.chess.engine.player.AI;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import com.chess.engine.board.Board;
+import com.chess.engine.board.Board.Builder;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Move.MoveFactory;
 import com.chess.engine.peices.Alliance;
+import com.chess.engine.peices.Piece;
+import com.chess.engine.player.MoveTransition;
 
 public final class Evaluator {
   
 	public static long evaluate(final Board board,final Alliance side) {
-	  return (Long) null;
+
+	  return  side.direction() *  pieceScore(board,side) + legalMovesScore(board,side);
   }
+	
+	public static Move getBestMove(final Board board, final Alliance side) {
+		final Collection<Move> moves = side.isBlack()?board.bPlayer().getLegalMoves(): board.wPlayer().getLegalMoves();
+		long max = Long.MIN_VALUE;
+		long cur = Long.MIN_VALUE;
+		Move curBest = MoveFactory.getNullMove();
+		
+		for(final Move move: moves) {	
+			final MoveTransition transition = board.curPlayer().makeMove(move);
+			if(transition.getMoveStatus().isDone()) {
+				cur = evaluate(board,side);
+				if(cur > max) {
+					max = cur;
+					curBest = move;
+				}
+		   }
+		}
+		
+		return curBest;
+	}
+	
+	private static long pieceScore(final Board board, final Alliance side) {
+		long sum = 0;
+		
+		final Collection<Piece> pieces = side.isBlack()? board.getBlackPieces():board.getWhitePieces();
+		
+		for(final Piece piece: pieces) {
+			sum += piece.getVal();
+		}
+		
+		return sum;
+	}
+	
+	private static long legalMovesScore(final Board board, final Alliance side) {
+		long sum = 0;
+		final Collection<Move> moves = side.isBlack()?board.bPlayer().getLegalMoves():board.wPlayer().getLegalMoves();
+		
+		for(final Move move: moves) {
+			if(move.isAttack()) {
+				sum += move.getAttackPiece().getVal();
+			}else {
+				sum += 100;
+			}
+		}
+		return sum;
+	}
 }
--- NQueens/engine/player/AI/MiniMax.java
+++ NQueens/engine/player/AI/MiniMax.java
@@ -1,11 +1,98 @@
 package com.chess.engine.player.AI;
 
+import java.util.Collection;
+
 import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Move.MoveFactory;
+import com.chess.engine.peices.Alliance;
+import com.chess.engine.player.MoveTransition;
 
 public class MiniMax {
-	private int depth;
+	private int maxDepth;
+	private int curDepth;
 	private Board curBoard;
 	private long alpha;
 	private long beta;
 	
+	
+	public MiniMax(final int depth, final Board board, final long alpha, final long beta) {
+		this.maxDepth = depth;
+		this.curBoard = board;
+		this.alpha = alpha;
+		this.beta = beta;
+	}
+	
+	public Move search(final Alliance side) {
+		
+	final Collection<Move> moves = (Collection<Move>) this.curBoard.getAllLegalMoves();
+	
+     Move best = MoveFactory.getNullMove();
+     long aplha = Long.MAX_VALUE; // min
+     long beta = Long.MIN_VALUE; // max
+     long cur = 0;
+     for(final Move move: moves) {
+    	 final MoveTransition transition = curBoard.curPlayer().makeMove(move);
+    	 if(transition.getMoveStatus().isDone()) {
+    		 if(curBoard.curPlayer().getAlliance().isWhite()) {
+    			 cur = min(this.maxDepth - 1,transition.getToBoard());
+    			 aplha = cur <= aplha ? cur : alpha;
+    			 best = move;
+    		 }else {
+    			 cur = max(this.maxDepth - 1, transition.getToBoard());
+    			 beta = cur >= beta ? cur : beta;
+    			 best = move;
+    		 }
+    	 }
+     }
+		
+		return best;
+	}
+	
+	
+	private long min(final int depth, final Board board) {
+		if(depth == 0) {
+			return Evaluator.evaluate(board, Alliance.WHITE);
+		}
+		
+		long curMin = Long.MAX_VALUE;
+		final Collection<Move> moves = board.curPlayer().getLegalMoves();
+		
+		for(final Move move: moves) {
+			final MoveTransition transition = board.curPlayer().makeMove(move);
+			
+			if(transition.getMoveStatus().isDone()) {
+				final long curVal = max(depth-1,board);
+				
+				if(curVal <= curMin) {
+					curMin = curVal;
+				}
+			}
+			
+		}
+		 return curMin;
+	}
+	
+	private long max(final int depth,final Board board) {
+		if(depth == 0) {
+			return Evaluator.evaluate(board, Alliance.BLACK);	
+		}
+		
+		long curMax = Long.MIN_VALUE;
+		final Collection<Move> moves = board.curPlayer().getLegalMoves();
+		
+		for(final Move move: moves) {
+			final MoveTransition transition = board.curPlayer().makeMove(move);
+			
+			if(transition.getMoveStatus().isDone()) {
+				final long curVal = min(depth-1,board);
+				
+				if(curVal >= curMax) {
+					curMax = curVal;
+				}
+			}
+			
+		}
+		 return curMax;
+	}
 }
