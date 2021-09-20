8a8c77cb6faac72ab04a16b2498d6db0b9ca6bb3
Andres Carrillo <andresc@cpp.edu>
2021-09-19 21:04:55 -0700

    Add files via upload

--- NQueens/engine/EventHandling/BoardListener.java
+++ NQueens/engine/EventHandling/BoardListener.java
@@ -4,5 +4,5 @@ import com.chess.engine.board.Move;
 import com.chess.engine.player.MoveTransition;
 
 public interface BoardListener {
-	public Move onOpponentMoveMade();
+	public void moveMade();
 }
